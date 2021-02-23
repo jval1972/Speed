09f0aa8b4a55bbc90f14951148e9d535db360728
jval1972 <jvalavanis@gmail.com>
2021-02-23 19:09:04 +0200

    Added MT_CAMERA actor definition

--- make_wad.cmd
+++ make_wad.cmd
@@ -1,4 +1,4 @@
-make_speeddef -o SCRIPT\actordef.inc -i WADSRC\SPEED\BASEDECORATIONS.txt -i WADSRC\SPEED\PARTICLES.txt -i WADSRC\SPEED\PATH.txt -i WADSRC\SPEED\SPEEDALIAS.txt
+make_speeddef -o SCRIPT\actordef.inc -i WADSRC\SPEED\BASEDECORATIONS.txt -i WADSRC\SPEED\CAMERA.txt -i WADSRC\SPEED\PARTICLES.txt -i WADSRC\SPEED\PATH.txt -i WADSRC\SPEED\SPEEDALIAS.txt
 cd .\WADSRC
 "C:\Program Files\7-Zip\7z.exe" a -r ..\..\bin\SPEED.zip *.*
 move ..\..\bin\SPEED.zip ..\..\bin\SPEED.pk3
