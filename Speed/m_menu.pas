08aaa1af32eab3763740403125ed89d2402c5032
jval1972 <jvalavanis@gmail.com>
2021-03-16 09:09:43 +0200

    More distinguisable navigate indicators for the menu

--- Speed/m_menu.pas
+++ Speed/m_menu.pas
@@ -3257,12 +3257,16 @@ begin
   end;
 
   if currentMenu.leftMenu <> nil then
+  begin
+    M_WriteText(6, 44, '<<', ma_left, @hu_fontB, @hu_fontB);
     M_WriteText(5, 44, '<<', ma_left, @hu_fontG, @hu_fontB);
+  end;
 
   if currentMenu.rightMenu <> nil then
   begin
     rstr := '>>';
     rlen := M_StringWidth(rstr, @hu_fontY);
+    M_WriteText(315 - rlen + 1, 44, rstr, ma_left, @hu_fontB, @hu_fontB);
     M_WriteText(315 - rlen, 44, rstr, ma_left, @hu_fontG, @hu_fontB);
   end;
 
